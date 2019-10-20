c0589d904c3081bd3fee078dcc395b456e9ab4d6
DennisBorst <dennis.borst@hotmail.nl>
2019-10-20 11:32:21 +0200

    Commit 4

---
+++ Blobtricity/Assets/PlayerControls.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private Camera mapCamera;
+
+    private bool IsCheckingMap;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckMap();
+        ChangeCamera();
+    }
+
+    private void CheckMap()
+    {
+        if (Input.GetKeyUp(KeyCode.M))
+        {
+            SwitchBool();
+        }
+    }
+
+    private void SwitchBool()
+    {
+        IsCheckingMap = !IsCheckingMap;
+    }
+
+    private void ChangeCamera()
+    {
+        if (IsCheckingMap)
+        {
+            mainCamera.enabled = false;
+            mapCamera.enabled = true;
+
+        }
+        else
+        {
+            mainCamera.enabled = true;
+            mapCamera.enabled = false;
+
+
+        }
+    }
+
+}
